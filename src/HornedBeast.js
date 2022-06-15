884dab4e4f0b56dc6e7d4a6a73757a01ccb7dced
Kris Dunning <dunning.kris@gmail.com>
2022-06-14 17:46:44 -0700

    wrapped up bootstrap implementation, strecth goals only remaining features to add

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card'
 import './HornedBeast.css';
-
 
 class HornedBeast extends React.Component{
   constructor(props){
@@ -20,16 +20,28 @@ class HornedBeast extends React.Component{
 
   render(){
     return (        
-      <article>
-        <h2>{this.props.title}</h2>
-        <div>
-          <p>❤️</p>
-          <p> Likes: {this.state.likes}</p>
-        </div>
-      <Image src={this.props.image_url} alt={this.props.title} title={this.props.title} onClick={this.handleLikes} fluid rounded/> 
-      <p>{this.props.desc}</p>
-      </article>
-    );
+      // <article>
+      //   <h2>{this.props.title}</h2>
+      //   <div>
+      //     <p>❤️</p>
+      //     <p> Likes: {this.state.likes}</p>
+      //   </div>
+      // <Image src={this.props.image_url} alt={this.props.title} title={this.props.title} onClick={this.handleLikes} fluid rounded/> 
+      // <p>{this.props.desc}</p>
+      // </article>
+    
+      <Card bg="info" style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={this.props.image_url} />
+      <Card.Body>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>
+          {this.props.desc}
+        </Card.Text>
+        <Button variant="secondary" onClick={this.handleLikes}>UpBoop the Snoot</Button>
+        <p>❤️ {this.state.likes} UpBoop's </p>
+      </Card.Body>
+    </Card>
+      );
   };
 }
 
